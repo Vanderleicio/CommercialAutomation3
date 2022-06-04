2eb323302b3e4b686d0135140aaafdfa72d38126
Alana Sampaio <71518539+AlanaSampaio@users.noreply.github.com>
2022-06-04 18:45:55 -0300

    login

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,41 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginController {
 
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private ImageView imageLogo;
+
+    @FXML
+    private AnchorPane nickFieldAndNickText;
+
+    @FXML
+    private Label nickname;
+
+    @FXML
+    private AnchorPane passFieldAndTextField;
+
+    @FXML
+    private Label password;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField textField;
+
+    public void loginUsers() {
+    	
+    	
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module CommercialAutomation3 {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens controller to javafx.graphics, javafx.fxml;
 }
